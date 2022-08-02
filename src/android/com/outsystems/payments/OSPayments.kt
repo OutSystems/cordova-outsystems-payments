422712e710eb9be11ac89417a3ba1966d117f1a9
alexgerardojacinto <alexandre.jacinto@outsystems.com>
2022-08-02 15:09:19 +0100

    feat: first version of bridge in Kotlin and other setup steps
    References: https://outsystemsrd.atlassian.net/browse/RMET-1721

---
+++ src/android/com/outsystems/payments/OSPayments.kt
@@ -0,0 +1,46 @@
+package com.outsystems.payments
+
+import android.content.Intent
+import org.apache.cordova.CallbackContext
+import com.outsystems.plugins.oscordova.CordovaImplementation
+import org.apache.cordova.CordovaInterface
+import org.apache.cordova.CordovaWebView
+import org.json.JSONArray
+import org.json.JSONObject
+
+class OSPayments : CordovaImplementation() {
+
+    override fun initialize(cordova: CordovaInterface, webView: CordovaWebView) {
+        super.initialize(cordova, webView)
+    }
+
+
+    override fun execute(action: String, args: JSONArray, callbackContext: CallbackContext): Boolean {
+        super.execute(action, args, callbackContext)
+        if (action == "setupConfiguration") {
+            val message: String = args.getString(0)
+            this.setupConfiguration(message, callbackContext)
+            return true
+        }
+        return false
+    }
+
+    private fun setupConfiguration(message: String?, callbackContext: CallbackContext) {
+        paymentsController.requestPayment(getActivity())
+    }
+
+    override fun onRequestPermissionResult(requestCode: Int,
+                                           permissions: Array<String>,
+                                           grantResults: IntArray) {
+        // Does nothing. These permissions are not required on Android.
+    }
+
+    override fun areGooglePlayServicesAvailable(): Boolean {
+        // Not used in this project.
+        return false
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+        super.onActivityResult(requestCode, resultCode, intent)
+    }
+}
